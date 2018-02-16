9dfad772a0371cc081c04a65f26a606c3124dff5
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-02-16 19:53:00 +0100

    Clean

--- controllers/BackendController.php
+++ controllers/BackendController.php
@@ -11,6 +11,7 @@ namespace humhub\modules\onlydocuments\controllers;
 use Yii;
 use yii\web\HttpException;
 use yii\helpers\Url;
+use yii\db\Expression;
 use humhub\modules\file\models\File;
 use humhub\modules\file\libs\FileHelper;
 use humhub\components\Controller;
@@ -45,7 +46,6 @@ class BackendController extends Controller
      */
     public function actionDownload()
     {
-        //Yii::warning("Downloading file guid: " . $this->file->guid, 'onlydocuments');
         return Yii::$app->response->sendFile($this->file->store->get(), $this->file->file_name);
     }
 
@@ -53,63 +53,57 @@ class BackendController extends Controller
     {
         Yii::$app->response->format = 'json';
 
-        $_trackerStatus = array(
+        $_trackerStatus = [
             0 => 'NotFound',
             1 => 'Editing',
             2 => 'MustSave',
             3 => 'Corrupted',
             4 => 'Closed',
             6 => 'ForceSave'
-        );
+        ];
 
         $result = [];
         $result['status'] = 'success';
-        $result["error"] = 0;
+        $result['error'] = 0;
 
         if (($body_stream = file_get_contents('php://input')) === FALSE) {
-            $result["error"] = "Bad Request";
+            $result['error'] = 'Bad Request';
             Yii::error('Bad tracker request', 'onlydocuments');
             return $result;
         }
 
-        $data = json_decode($body_stream, TRUE); //json_decode - PHP 5 >= 5.2.0
+        $data = json_decode($body_stream, TRUE);
         if ($data === NULL) {
             Yii::error('Got bad tracking response from documentserver!', 'onlydocuments');
-            $result["error"] = "Bad Response";
+            $result['error'] = 'Bad Response';
             return $result;
         }
 
-        //Yii::warning('Tracking request for file ' . $this->file->guid . ' - data: ' . print_r($data, 1), 'onlydocuments');
-
-        $status = $_trackerStatus[$data["status"]];
+        $status = $_trackerStatus[$data['status']];
         switch ($status) {
-            case "MustSave":
-            case "Corrupted":
-            case "ForceSave":
-
-                $newData = file_get_contents($data["url"]);
+            case 'MustSave':
+            case 'Corrupted':
+            case 'ForceSave':
+                $newData = file_get_contents($data['url']);
 
                 if (!empty($newData)) {
                     $this->file->getStore()->setContent($newData);
 
                     if ($status != 'ForceSave') {
-                        $this->file->updateAttributes(['onlydocuments_key' => new \yii\db\Expression('NULL')]);
-                        //Yii::warning('Dosaved', 'onlydocuments');
+                        $this->file->updateAttributes(['onlydocuments_key' => new Expression('NULL')]);
                     } else {
-                        //Yii::warning('ForceSaved', 'onlydocuments');
                     }
                     $saved = 1;
                 } else {
-                    Yii::error('Could not save onlyoffice document: ' . $data["url"], 'onlydocuments');
+                    Yii::error('Could not save onlyoffice document: ' . $data['url'], 'onlydocuments');
                     $saved = 0;
                 }
 
-                $result["c"] = "saved";
-                $result["status"] = $saved;
+                $result['c'] = 'saved';
+                $result['status'] = $saved;
                 break;
         }
 
-        //Yii::warning("Return: " . print_r($result, 1), 'onlydocuments');
         return $result;
     }
 
